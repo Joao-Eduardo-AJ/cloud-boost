319fc11a52a50a890b4f2fa050fe347a8785ac4a
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2024-02-28 23:56:08 -0300

    feat: adds header to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { MenuHamburger } from "@/components";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>hello World!</h1>
-      <p>Yeah! this font is inter</p>
+    <main className="px-7 pt-4">
+      <header>
+        <div className="border-gray-850 md:h-18 flex h-16 w-full items-center justify-between rounded-full border bg-gray-900 px-6">
+          <Image
+            alt="logo"
+            width={167}
+            height={31}
+            src="images/logo.svg"
+            className="h-7 w-36 md:h-8 md:w-44"
+          />
+          <MenuHamburger />
+        </div>
+      </header>
+      <section></section>
+      <footer></footer>
     </main>
   );
 }
