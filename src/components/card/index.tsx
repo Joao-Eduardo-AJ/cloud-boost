1a6ab453fe81af1e5bbee30b5968acace83c03a9
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2024-03-13 12:28:38 -0300

    styles: update text align in card component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,7 +7,7 @@ interface IProps {
 }
 
 export const Card = ({ src, text, title }: IProps) => (
-  <article className="flex max-w-[264px] flex-col items-center lg:items-start lg:text-start">
+  <article className="flex max-w-[264px] flex-col items-center text-center lg:items-start lg:text-start">
     <Image src={src} alt="benefits" width={48} height={48} />
     <h3 className="text-lg font-bold text-gray-100">{title}</h3>
     <p className="text-gray-600">{text}</p>
