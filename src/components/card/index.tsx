e961b5c46b2164f67a4efb1cafd5fb47ef49e72d
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2024-03-12 23:11:39 -0300

    feat: adds card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+interface IProps {
+  src: string;
+  title: string;
+  text: string;
+}
+
+export const Card = ({ src, text, title }: IProps) => (
+  <article className="flex max-w-[264px] flex-col items-center lg:items-start lg:text-start">
+    <Image src={src} alt="benefits" width={48} height={48} />
+    <h3 className="text-lg font-bold text-gray-100">{title}</h3>
+    <p className="text-gray-600">{text}</p>
+  </article>
+);
