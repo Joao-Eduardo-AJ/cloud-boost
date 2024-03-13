8e21cf6fbf5941a59f5b85dc662202e281780a29
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2024-03-12 23:11:53 -0300

    feat: adds second section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/components/Input";
+import { Card } from "@/components/card";
 import { Header } from "@/components/header";
 import { TextsProvider } from "@/translation";
 import Image from "next/image";
@@ -111,6 +112,36 @@ export default function Home() {
             height={200}
             src="images/mobile_aux_img.svg"
             className="sm:hidden"
+          />
+        </div>
+      </section>
+      <section className="mx-6 grid gap-16 border-b border-gray-850 py-18 md:mx-28">
+        <div className="flex flex-col justify-between gap-4 text-center lg:flex-row lg:gap-0 lg:text-start">
+          <h2 className="track text-3xl font-bold text-gray-100 md:text-4.5xl max-w-2xl">
+            {texts.SECOND_S_TITLE}
+          </h2>
+          <p className="font-normal text-gray-200">{texts.SECOND_S_SUB}</p>
+        </div>
+        <div className="flex flex-col items-center gap-16 lg:flex-row lg:items-start lg:justify-between lg:gap-0">
+          <Card
+            src="images/user.svg"
+            title={texts.CARD_1_TITLE}
+            text={texts.CARD_1_TEXT}
+          />
+          <Card
+            src="images/wallet.svg"
+            title={texts.CARD_2_TITLE}
+            text={texts.CARD_2_TEXT}
+          />
+          <Card
+            src="images/checked.svg"
+            title={texts.CARD_3_TITLE}
+            text={texts.CARD_3_TEXT}
+          />
+          <Card
+            src="images/boost.svg"
+            title={texts.CARD_4_TITLE}
+            text={texts.CARD_4_TEXT}
           />
         </div>
       </section>
